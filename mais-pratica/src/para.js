7c8c17197ea7cb12df28f1bb7b92891126c3e9e4
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-18 08:38:20 -0300

    c21- adding new test of code

--- mais-pratica/src/para.js
+++ mais-pratica/src/para.js
@@ -2,8 +2,12 @@ import React from "react"
 
 const Paragraph = (props) =>{
     return(
+      <>
       <p>{props.text}
        </p>
+
+       <h3>thought and practice more and more about react</h3>
+      </> 
     )
   
   }
